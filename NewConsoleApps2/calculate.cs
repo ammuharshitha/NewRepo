c3d8a02f5256ab996a73e1ca16e3bcd6feeae232
Harshitha <s.harshitha@sonata-software.com>
2022-08-24 18:32:14 +0530

    new code

---
+++ NewConsoleApps2/calculate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewConsoleApps2
+{
+    public class calculate
+    {
+        public int Add(int a , int b)
+        {
+            return a + b;   
+        }
+
+
+    }
+}
